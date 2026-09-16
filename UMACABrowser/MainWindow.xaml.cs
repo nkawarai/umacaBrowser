1f4e1f55109ce017a6cd0c340977038a64eb660b
Nobuyuki Kawarai <studio105.xxx@gmail.com>
2026-09-16 14:40:50 +0900

    新規作成。

---
+++ UMACABrowser/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace UMACABrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
