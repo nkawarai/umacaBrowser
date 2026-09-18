0efeadb0e7e1b0034f69b0a2f435df4021d21cd8
Nobuyuki Kawarai <studio105.xxx@gmail.com>
2026-09-19 08:44:05 +0900

    東京、中山、京都、阪神の1時間天気を追加。

--- SmartRCEx/MainWindow.xaml.cs
+++ SmartRCEx/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ namespace SmartRCEx
         private const string JraUrl = "https://www.jra.go.jp/";
         private const string JRAYoutubeUrl = "https://www.youtube.com/@jraofficial/featured";
         private const string KeibaSalonUrl = "https://www.keiba-salon.com/";
+        private const string WeatherTokyo = "https://tenki.jp/leisure/horse/3/16/32005/1hour.html";
+        private const string WeatherNakayama = "https://tenki.jp/leisure/horse/3/15/32004/1hour.html";
+        private const string WeatherKyoto = "https://tenki.jp/leisure/horse/6/29/32007/1hour.html";
+        private const string WeatherHanshin = "https://tenki.jp/leisure/horse/6/31/32008/1hour.html";
 
         public MainWindow()
         {
@@ -36,6 +40,26 @@ namespace SmartRCEx
         private void KeibaSalonButton_Click(object sender, RoutedEventArgs e)
         {
             RightBrowser.Address = KeibaSalonUrl;
+        }
+
+        private void WeatherTokyoButton_Click(object sender, RoutedEventArgs e)
+        {
+            RightBrowser.Address = WeatherTokyo;
+        }
+
+        private void WeatherNakayamaButton_Click(object sender, RoutedEventArgs e)
+        {
+            RightBrowser.Address = WeatherNakayama;
+        }
+
+        private void WeatherKyotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            RightBrowser.Address = WeatherKyoto;
+        }
+
+        private void WeatherHanshinButton_Click(object sender, RoutedEventArgs e)
+        {
+            RightBrowser.Address = WeatherHanshin;
         }
     }
 }
